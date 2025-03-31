cb1ce4ca6c39234c199761904a9812871ca47198
Antonetwork <Pardoytmc@gmail.com>
2025-03-31 09:38:33 +0200

    Update service.py

--- grpc_service/service.py
+++ grpc_service/service.py
@@ -1,7 +1,7 @@
 import grpc
 from concurrent import futures
-import egapro_pb2
-import egapro_pb2_grpc
+import proto.egapro_pb2 as egapro_pb2
+import proto.egapro_pb2_grpc as egapro_pb2_grpc
 import csv
 
 class EgaproService(egapro_pb2_grpc.EgaproServiceServicer):
@@ -11,12 +11,10 @@ class EgaproService(egapro_pb2_grpc.EgaproServiceServicer):
     def load_data(self):
         data = []
         csv_path = "/app/data/index-egalite-fh-utf8.csv"
-        try:
-            with open(csv_path, newline='', encoding='utf-8') as csvfile:
-                reader = csv.DictReader(csvfile, delimiter=';')
-                data = [row for row in reader]
-        except Exception as e:
-            print(f"❌ Erreur de chargement CSV: {e}")
+        with open(csv_path, newline='', encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                data.append(row)
         return data
 
     def GetEntreprises(self, request, context):
@@ -25,21 +23,21 @@ class EgaproService(egapro_pb2_grpc.EgaproServiceServicer):
 
     def GetEntrepriseBySiren(self, request, context):
         siren = request.siren
-        entreprise = next((e for e in self.data if e['siren'] == siren), None)
+        entreprise = next((e for e in self.data if e["siren"] == siren), None)
         if entreprise:
             return egapro_pb2.EntrepriseResponse(**entreprise)
         else:
             context.set_code(grpc.StatusCode.NOT_FOUND)
-            context.set_details('Entreprise non trouvée')
+            context.set_details("Entreprise non trouvée")
             return egapro_pb2.EntrepriseResponse()
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     egapro_pb2_grpc.add_EgaproServiceServicer_to_server(EgaproService(), server)
-    server.add_insecure_port('[::]:50051')
+    server.add_insecure_port("[::]:50051")
     server.start()
-    print("✅ gRPC Server lancé sur le port 50051")
+    print("✅ Serveur gRPC en cours d'exécution sur le port 50051")
     server.wait_for_termination()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     serve()
